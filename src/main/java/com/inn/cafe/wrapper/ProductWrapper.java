3b4bc5b5c786a0cc0e6e6dac02d86fa8f0166383
Shahab <breakkingsha@gmail.com>
2024-01-28 23:57:40 +0300

    Add Product methods(delete, findById, updateStatus, findByCategoryId)

--- src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
+++ src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
@@ -18,4 +18,22 @@ public class ProductWrapper {
     Integer categoryId;
     String categoryName;
 
+
+    public ProductWrapper(Integer id, String name) {
+        this.id =id;
+        this.name=name;
+    }
+
+    public ProductWrapper(Integer id, String name, String description, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 }
+
+
+
+
+
+
